4344ca7520a371143618299d07d8634cb48bfe0e
Mario Cecchi <macecchi@gmail.com>
2015-11-09 17:48:10 -0200

    Calculating average return times

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ MongoClient.connect(url, function(err, db) {
 				var countStops = 0;
 
 				line.spots.forEach(function(bus_stop) {
+					var busStopHistory = [];
+					
 					riobus.findBusesCloseToCoordinate(db, line.line, bus_stop.longitude, bus_stop.latitude, function(matches) {
 						console.log("- [" + ++countStops + "/" + totalBusStops + "] " + matches.length + " buses close to [" + bus_stop.latitude + ", " + bus_stop.longitude + "]");
 
@@ -39,6 +41,7 @@ MongoClient.connect(url, function(err, db) {
 					    	var duplicated;
 					    	var minutesDiff = 99999;
 
+					    	// See if the bus is a duplicate (if it's the same order in the same place in the same time interval)
 					    	if (bus.order == previousMatch.order) {
 					    		previousMatches.push(previousMatch);
 					    		previousMatches.forEach(function(pastMatch) {
@@ -49,16 +52,54 @@ MongoClient.connect(url, function(err, db) {
 					    	else {
 					    		previousMatches = [];
 					    		minutesDiff = 99999;
+					    		duplicated = false;
 					    	}
 
-					     	console.log((!duplicated ? "--" : "---") + " " + bus.order + " with distance " + Math.ceil(bus.dist.calculated) + "m (bus: " + bus.dist.location + " @ " + time.toLocaleString() + ")");
+					    	if (!duplicated) {
+					    		busStopHistory.push(bus);
+					     		console.log("-- " + bus.order + " with distance " + Math.ceil(bus.dist.calculated) + "m (bus: " + bus.dist.location + " @ " + time.toLocaleString() + ")");
+					    	}
+					    	else {
+					     		// console.log("--- " + bus.order + " with distance " + Math.ceil(bus.dist.calculated) + "m (bus: " + bus.dist.location + " @ " + time.toLocaleString() + ")");
+					    	}
+
 					    	previousMatch = bus;
 					    });
 
+					    // Calculate time between buses
+					    console.log('\nStatistics:');
+						previousMatch = {};
+						var timeDiffSum = 0, timeDiffN = 0;
+						busStopHistory.forEach(function(bus) {
+							if (bus.order == previousMatch.order) {
+								var timeDiffFromLast = Math.round(Math.abs(new Date(bus.timestamp) - new Date(previousMatch.timestamp))/1000/60);
+								// Ignore buses with time interval bigger than 4 hours
+								if (timeDiffFromLast < 4*60) {
+									timeDiffSum += timeDiffFromLast;
+									timeDiffN++;
+								}
+							}
+							else {
+								if (timeDiffN > 0) {
+									var timeDiffAverage = timeDiffSum/timeDiffN;
+									var timeDiffHours = Math.floor(timeDiffAverage / 60);
+									var timeDiffMinutes = Math.floor(timeDiffAverage % 60);
+
+									console.log('Average return time for order ' + previousMatch.order + ': ' + timeDiffHours + 'h' + timeDiffMinutes + 'min');
+								}
+								timeDiffSum = 0;
+								timeDiffN = 0;
+							}
+							previousMatch = bus;
+
+						});
+
 					    if (matches.length > 0) console.log('');
 					    if (countStops == totalBusStops) process.exit(0);
-		 			});
-	 			});
+		 			}); // end matches loop
+
+					
+	 			}); // end bus stops loop
 			});
 		});
 	});
