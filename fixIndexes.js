54893c6a3eeff1de32377dc9f7057c2fcac7fd1c
Mario Cecchi <macecchi@gmail.com>
2015-12-09 17:44:25 -0200

    Fix fixIndexes.js

--- fixIndexes.js
+++ fixIndexes.js
@@ -1,14 +1,14 @@
-var MongoClient = require('mongodb').MongoClient;
+/* global process; */
 var assert = require('assert');
-var url = 'mongodb://localhost:27017/riobus';
-var riobus = require('./operations');
+var Config = require('./config');
+var RioBus = require('./operations');
 
-MongoClient.connect(url, function(err, db) {
+RioBus.connect(function(err, db) {
 	assert.equal(null, err);
 
 	console.log('Redefining indexes...');
-	riobus.ensureIndexes(db, 'bus_history_old', function() {
-		console.log('Indexes are set.');
-		process.exit() ;
+	RioBus.ensureIndexes(db, Config.schema.busHistoryCollection, function() {
+		console.log('Indexes are set for collection ' + Config.schema.busHistoryCollection + '.');
+		process.exit();
 	});
 });
