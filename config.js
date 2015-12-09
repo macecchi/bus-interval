4c792c950d6b7495a3c8b375bfa0f8a65045dcec
Mario Cecchi <macecchi@gmail.com>
2015-12-09 17:45:56 -0200

    Fix interval timezone

--- config.js
+++ config.js
@@ -13,11 +13,11 @@ module.exports = {
     },
     schema: {
         busStopsCollection: 'bus_stop',
-        busHistoryCollection: 'bus_history_old',
+        busHistoryCollection: 'bus_history',
         busHistoryTemporaryCollection: 'bus_history_temp'
     },
     query: {
-        dateInterval: ["2015-11-24T00:00:00.000Z", "2015-11-25T00:00:00.000Z"],
+        dateInterval: ["2015-12-08T02:00:00.000Z", "2015-12-09T02:00:00.000Z"], // Time interval to query. Add 2 hours to fix timezone.
         maxDistance: 99, // Maximum distance a bus can be to a stop to be considered valid (meters)
         duplicatedBusTimeLimit: 15 // Maximum time a bus near the same area can be considered to be duplicated (minutes)
     }
