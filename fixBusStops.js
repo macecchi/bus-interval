89d7fc5f607e31eae07c0d843b53f35c638b3675
Mario Cecchi <macecchi@gmail.com>
2016-04-02 12:47:26 -0300

    Finishing script gracefully

--- fixBusStops.js
+++ fixBusStops.js
@@ -6,26 +6,13 @@ assert(process.argv.length > 2, 'Missing bus line parameter.');
 
 var lastArg = process.argv[process.argv.length-1];
 assert(!isNaN(lastArg), 'Missing bus line parameter.');
-var searchLine = lastArg;
+var line = lastArg;
 
-console.log('Searching with line ' + searchLine + '\n');
+console.log('Searching with line ' + line + '\n');
 
 RioBus.connect(function(err, db) {
 	assert.equal(null, err);
 
-    identifyBusStopDirectionsFromItinerary(db, searchLine, function() {
-        process.exit(0);
-    });
-});
-
-function identifyBusStopDirectionsFromItinerary(db, line, callback) {
-    prepareLineForMatchingSpots(db, line, function(results) {
-        console.log(results);
-        callback();
-    });
-}
-
-function prepareLineForMatchingSpots(db, line, callback) {
 	db.collection('itinerary').aggregate([
         { "$match": { "line": line } },
         { "$unwind": "$spots" },
@@ -35,6 +22,7 @@ function prepareLineForMatchingSpots(db, line, callback) {
 	 	assert.equal(err, null);
         db.collection('stops_itinerary_temp').createIndex({ "coordinates": "2dsphere" }, null, function(err, results) {
             assert.equal(err, null);
+            
             db.collection('stops_itinerary_temp').find({}).toArray(function(err, itinerarySpots) {
                 db.collection('bus_stop').find({ "line": line }).toArray(function(err, linesInfo) {
                     assert.equal(err, null);
@@ -43,22 +31,26 @@ function prepareLineForMatchingSpots(db, line, callback) {
                     var processedSpots = [];
                     for (var i=0; i<lineInfo.spots.length; i++) {
                         var stopSpot = lineInfo.spots[i];
+                        
                         processAndUpdateStop(db, stopSpot, function(stopSpotProcessed) {
                             console.log('Bus stop processed: ', stopSpotProcessed);
                             processedSpots.push(stopSpotProcessed);
+                            
                             if (processedSpots.length == lineInfo.spots.length) {
                                 lineInfo.spots = processedSpots;
                                 updateLineSpots(db, lineInfo, function() {
-                                    callback();
+                                    console.log('Done processing ' + processedSpots.length + ' bus stops.');
+                                    process.exit(0);
                                 });
                             }
                         });
                     }
+                    
                 });
             });
         });
  	});
-}
+});
 
 function findItinerarySpotClosestToCoordinate(db, longitude, latitude, callback) {
 	db.collection('stops_itinerary_temp').aggregate([
