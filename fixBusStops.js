df72962cf9465a039c3fcd0f136e0b05a0a5148b
Mario Cecchi <macecchi@gmail.com>
2016-04-02 18:13:39 -0300

    Refactor

--- fixBusStops.js
+++ fixBusStops.js
@@ -7,13 +7,42 @@ assert(process.argv.length > 2, 'Missing bus line parameter.');
 var lastArg = process.argv[process.argv.length-1];
 assert(!isNaN(lastArg), 'Missing bus line parameter.');
 var line = lastArg;
+var db;
 
 console.log('Searching with line ' + line + '\n');
 
-RioBus.connect(function(err, db) {
+RioBus.connect(function(err, _db) {
 	assert.equal(null, err);
+    db = _db;
 
-	db.collection('itinerary').aggregate([
+	prepareLineForProcessing(line, function() {
+        findBusStopsForLine(line, function(lineInfo) {
+            var processedSpots = [];
+            
+            for (var i=0; i<lineInfo.spots.length; i++) {
+                var stopSpot = lineInfo.spots[i];
+                
+                processAndUpdateStop(stopSpot, function(stopSpotProcessed) {
+                    console.log('Bus stop processed: ', stopSpotProcessed);
+                    processedSpots.push(stopSpotProcessed);
+                    
+                    if (processedSpots.length == lineInfo.spots.length) {
+                        lineInfo.spots = processedSpots;
+                        updateLineSpots(lineInfo, function() {
+                            console.log('Done processing ' + processedSpots.length + ' bus stops.');
+                            process.exit(0);
+                        });
+                    }
+                });
+            }
+            
+        });
+    });
+    
+});
+
+function prepareLineForProcessing(line, callback) {
+    db.collection('itinerary').aggregate([
         { "$match": { "line": line } },
         { "$unwind": "$spots" },
         { "$project": { "_id": false, "returning": "$spots.returning", "coordinates": ["$spots.latitude", "$spots.longitude"] } },
@@ -22,37 +51,19 @@ RioBus.connect(function(err, db) {
 	 	assert.equal(err, null);
         db.collection('stops_itinerary_temp').createIndex({ "coordinates": "2dsphere" }, null, function(err, results) {
             assert.equal(err, null);
-            
-            db.collection('stops_itinerary_temp').find({}).toArray(function(err, itinerarySpots) {
-                db.collection('bus_stop').find({ "line": line }).toArray(function(err, linesInfo) {
-                    assert.equal(err, null);
-                    var lineInfo = linesInfo[0];
-                    
-                    var processedSpots = [];
-                    for (var i=0; i<lineInfo.spots.length; i++) {
-                        var stopSpot = lineInfo.spots[i];
-                        
-                        processAndUpdateStop(db, stopSpot, function(stopSpotProcessed) {
-                            console.log('Bus stop processed: ', stopSpotProcessed);
-                            processedSpots.push(stopSpotProcessed);
-                            
-                            if (processedSpots.length == lineInfo.spots.length) {
-                                lineInfo.spots = processedSpots;
-                                updateLineSpots(db, lineInfo, function() {
-                                    console.log('Done processing ' + processedSpots.length + ' bus stops.');
-                                    process.exit(0);
-                                });
-                            }
-                        });
-                    }
-                    
-                });
-            });
+            callback();
         });
- 	});
-});
+    });
+}
 
-function findItinerarySpotClosestToCoordinate(db, longitude, latitude, callback) {
+function findBusStopsForLine(line, callback) {
+    db.collection('bus_stop').find({ "line": line }).toArray(function(err, linesInfo) {
+        assert.equal(err, null);
+        callback(linesInfo[0]);
+    });
+}
+
+function findItinerarySpotClosestToCoordinate(longitude, latitude, callback) {
 	db.collection('stops_itinerary_temp').aggregate([
         { 
             "$geoNear": { 
@@ -77,8 +88,8 @@ function findItinerarySpotClosestToCoordinate(db, longitude, latitude, callback)
  	});
  }
  
-function processAndUpdateStop(db, stopSpot, callback) {
-    findItinerarySpotClosestToCoordinate(db, stopSpot.longitude, stopSpot.latitude, function(match) {
+function processAndUpdateStop(stopSpot, callback) {
+    findItinerarySpotClosestToCoordinate(stopSpot.longitude, stopSpot.latitude, function(match) {
         if (match) {
             stopSpot.returning = match.returning;
         }
@@ -86,7 +97,7 @@ function processAndUpdateStop(db, stopSpot, callback) {
     });
 }
 
-function updateLineSpots(db, lineDocument, callback) {
+function updateLineSpots(lineDocument, callback) {
     db.collection('bus_stop').update({ "_id": lineDocument._id }, lineDocument, function(err, result) {
 	 	assert.equal(err, null);
      	callback();
