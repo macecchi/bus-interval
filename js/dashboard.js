e24b3d0867fc41cf8bc36c8281c734c313fafb3c
Mario Cecchi <macecchi@gmail.com>
2016-06-01 23:39:47 -0300

    Refactor

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,66 +1,63 @@
-var graph_container = '#graph';
+var graphContainer = '#graph';
+var date = '2016-04-13';
+var statsFolder = 'stats';
+var line = '325';
 
 var margin = {top: 10, right: 0, bottom: 30, left: 40},
-    width = $(graph_container).width() - margin.left - margin.right,
-    height = $(graph_container).height() - margin.top - margin.bottom;
+    width = $(graphContainer).width() - margin.left - margin.right,
+    height = $(graphContainer).height() - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
+var y = d3.scale.linear().range([height, 0]);
+var xAxis = d3.svg.axis().scale(x).orient("bottom");
+var yAxis = d3.svg.axis().scale(y).orient("left");
 
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10, "");
-
-var svg = d3.select(graph_container).append("svg:svg")
+var svg = d3.select(graphContainer).append("svg:svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.json("stats/325.json", function(error, data) {
-  if (error) return console.warn(error);
-    var hourlyWaitTimes = []
-    var hours = [];
-    for (var hour=0; hour<24; hour++) {
-        hours.push(hour);
-        // hours.push(hour + '-' + (hour+1) + 'h00');
-        hourlyWaitTimes.push({ hour: hour, waitTime: data.hourlyAvgWaitTimes[hour]});
-    }
-  x.domain(hours);
-  y.domain([0, d3.max(data.hourlyAvgWaitTimes)]);
+processStats(line, date);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+function processStats(line, date) {
+  d3.json(statsFolder + '/' + date + '/' + line + '.json', function(error, data) {
+    if (error) return console.warn(error);
+      var hourlyWaitTimes = []
+      var hours = [];
+      for (var hour=0; hour<24; hour++) {
+          hours.push(hour);
+          // hours.push(hour + '-' + (hour+1) + 'h00');
+          hourlyWaitTimes.push({ hour: hour, waitTime: data.hourlyAvgWaitTimes[hour]});
+      }
+    x.domain(hours);
+    y.domain([0, d3.max(data.hourlyAvgWaitTimes)]);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Tempo de espera");
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  svg.selectAll(".bar")
-      .data(hourlyWaitTimes)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.hour); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.waitTime); })
-      .attr("height", function(d) { return height - y(d.waitTime); });
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Tempo de espera");
 
-      $('#avg-return-time').html(data.avgReturnTime + ' min');
-      $('#avg-wait-time').html(data.avgWaitTime + ' min');
-});
+    svg.selectAll(".bar")
+        .data(hourlyWaitTimes)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.hour); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.waitTime); })
+        .attr("height", function(d) { return height - y(d.waitTime); });
+
+        $('#avg-return-time').html(data.avgReturnTime + ' min');
+        $('#avg-wait-time').html(data.avgWaitTime + ' min');
+  });
+}
